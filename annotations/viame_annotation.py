2221d385f447df5b62b9721ed4d32801d7acdee5
Filippo Varini <filippo.varini20@imperial.ac.uk>
2024-02-08 15:06:08 +0000

    phase2 dataset complete

--- annotations/viame_annotation.py
+++ annotations/viame_annotation.py
@@ -11,7 +11,7 @@ class Viame(Annotation):
     return [xmin, ymin, xmax, ymax]
   
   def get_frame_id(self, row, source):
-    id = f"{source}_frame{int(row['3: Unique Frame Identifier'])*29}.jpg"
+    id = f"{source}_frame{int(row['3: Unique Frame Identifier'])*30}.jpg"
     return id
   
   def get_track_id(self, row):
